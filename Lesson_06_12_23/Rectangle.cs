e2b486fd0b95ca008e1f371f082774ff8f245ba9
Виталий Рублевский <vitrub76@gmail.com>
2023-12-11 18:46:19 +0200

    видалений та зроблений клас квадрат

---
+++ Lesson_06_12_23/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_06_12_23
+{
+    internal class Rectangle : Figur
+    {
+        public float SideA {  get; set; }
+        public float SideB { get; set; }
+
+        public Rectangle() : base() { }
+
+        public Rectangle(string Name, float SideA, float SideB) : base()
+        {
+            this.NameFigur = Name;
+            this.SideA = SideA;
+            this.SideB = SideB;
+            this.Perim = this.SideA * 2 + this.SideB * 2;
+            this.Area = this.SideA * this.SideB;
+        }
+
+        public override double Perimetr()
+        {
+            return this.SideA * 2 + this.SideB * 2;
+        }
+
+        public override double AreaFigur()
+        {
+            return this.SideA * this.SideB;
+        }
+    }
+}
--- Lesson_06_12_23/Square.cs
+++ Lesson_06_12_23/Square.cs
@@ -1,37 +1,34 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lesson_06_12_23
 {
     internal class Square : Figur
     {
-        public int CountSide { get; set; }  
-        public float Size { get; set; }
+        public  float SideSq {  get; set; }
 
         public Square() : base() { }
-        public Square(string Name, float Size) : base()
+
+        public Square(string Name, float SideSq) : base()
         {
             this.NameFigur = Name;
-            this.CountSide = 4;
-            this.Size = Size;
-            this.Perim = this.Size * this.CountSide;
-            this.Area = this.Size * this.Size;
+            this.SideSq = SideSq;
+            this.Perim = this.SideSq * 4;
+            this.Area = this.SideSq * this.SideSq;
+        }
+
+        public override double Perimetr()
+        {
+            return this.SideSq * 4;
+        }
+
+        public override double AreaFigur()
+        {
+            return this.SideSq * this.SideSq;
         }
 
     }
-    //   ????????????  ТОЛЬКО в этом классе!!!!
-    public override double Perimetr()
-    {
-        return this.CountSide * this.Size;
-    }
-
-    public override double AreaFigur()
-    {
-
-        return this.Size * this.Size;
-    }
-
-
-
-
-
 }
